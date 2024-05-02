6781491bd8c5090421139d45380ecb5edc435ed4
Sid-Taha <twahasiddqui11@gmail.com>
2024-05-03 02:17:33 +0500

    first commit

---
+++ Q13/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let tranprtation = ['Car', 'Bike', 'Bus', 'Private jet', 'UFO'];
+for (let index = 0; index < tranprtation.length; index++) {
+    console.log(`I would like to own a ` + tranprtation[index]);
+}
---
+++ Q13/index.ts
@@ -0,0 +1,8 @@
+
+let tranprtation: string[] = ['Car', 'Bike', 'Bus' , 'Private jet', 'UFO']
+
+for (let index = 0; index < tranprtation.length; index++) {
+  
+    console.log(`I would like to own a ` +tranprtation[index]);
+
+}
