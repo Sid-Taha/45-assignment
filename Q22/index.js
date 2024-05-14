10183f9be28530590e357567980a6aebd3e075a9
Sid-Taha <twahasiddqui11@gmail.com>
2024-05-15 02:03:55 +0500

    Q22 commit

---
+++ Q22/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let names_array = ["taha", "ahmed", "ali"];
+let a = names_array[3];
+let b = names_array[1];
+console.log(a);
+console.log(b);
---
+++ Q22/index.ts
@@ -0,0 +1,12 @@
+
+let names_array: string[] = ["taha", "ahmed", "ali"]
+
+let a = names_array[3]
+
+let b = names_array[1]
+
+console.log(a);
+console.log(b);
+
+
+
