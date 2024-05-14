65471626a6f6fb74334c43a195ca60847b7b868b
Sid-Taha <twahasiddqui11@gmail.com>
2024-05-15 02:27:05 +0500

    Q23 commit

---
+++ Q23/index.ts
@@ -0,0 +1,37 @@
+
+
+
+
+
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+
+console.log("Is car == 'toyota'? I predict false.")
+console.log(car == 'toyota')
+
+console.log("Is car == 'toyota'? I predict true.")
+console.log(car !== 'toyota')
+
+console.log("Is car == 'toyota'? I predict false.")
+console.log(car === 'toyota')
+
+console.log("Is car === 'suzuki'? I predict false.")
+console.log(car === 'suzuki')
+
+console.log("Is car !== 'aeroplane'? I predict true.")
+console.log(car !== 'aeroplane')
+
+console.log("Is car !== 'rikshaw'? I predict true.")
+console.log(car !== 'rikshaw')
+
+console.log("Is car === 'honda'? I predict false.")
+console.log(car === 'honda')
+
+console.log("Is car !== 'cycle'? I predict true.")
+console.log(car !== 'cycle')
+
+
