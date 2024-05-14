4a9194973f30d6bb694f9b63ec13a9c6b6251a7e
Sid-Taha <twahasiddqui11@gmail.com>
2024-05-15 01:47:03 +0500

    Q21 commit

---
+++ Q21/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let person = {
+    name: "taha",
+    age: 26,
+    isStudent: true
+};
+console.log(person.name);
---
+++ Q21/index.ts
@@ -0,0 +1,16 @@
+
+
+type Tperson = {
+    name:string
+    age: number
+    isStudent: boolean
+}
+
+
+let person: Tperson = {
+    name:"taha",
+    age : 26,
+    isStudent: true
+}
+
+console.log(person.name);
