6fe97f35f1e20a75da3270426b63a39aafa5e9fe
Sid-Taha <twahasiddqui11@gmail.com>
2024-05-16 03:32:54 +0500

    Q27

---
+++ Q27/index.ts
@@ -0,0 +1,29 @@
+
+
+let alien_color1 = "green"
+
+
+// version 1
+if(alien_color1 === "green"){
+    console.log("The player just earned 5 points.");
+}else{
+    console.log("The player just earned 10 points.");
+}
+
+
+let alien_color2 = "yellow"
+// version 2
+if(alien_color2 === "yellow"){
+    console.log("The player just earned 10 points.");
+}else{
+    console.log("The player just earned 5 points.");
+}
+
+
+let alien_color3 = "red"
+// version 3
+if(alien_color3 === "red"){
+    console.log("The player just earned 15 points.");
+}else{
+    console.log("The player just earned 5 points.");
+}
