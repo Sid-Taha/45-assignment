3ae145035b19e519d94881c8616c4a6d1ae169af
Sid-Taha <twahasiddqui11@gmail.com>
2024-05-18 01:36:18 +0500

    Q29

---
+++ Q29/index.js
@@ -0,0 +1,17 @@
+"use strict";
+let fruits = ["banana", "apple", "mango"];
+if (fruits.includes("melon")) {
+    console.log("i really like melon");
+}
+if (fruits.includes("banana")) {
+    console.log("i really like banana");
+}
+if (fruits.includes("apple")) {
+    console.log("i really like apple");
+}
+if (fruits.includes("mango")) {
+    console.log("i really like mango");
+}
+if (fruits.includes("kiwi")) {
+    console.log("i really like kiwi");
+}
