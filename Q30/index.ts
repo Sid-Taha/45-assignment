7f9fbc186d3b636830ee89ab7a2644fd60e2c4a7
Sid-Taha <twahasiddqui11@gmail.com>
2024-05-18 01:51:15 +0500

    Q30

---
+++ Q30/index.ts
@@ -0,0 +1,14 @@
+
+
+
+let username: string[] = ["ahmed", "admin", "taha", "asad", "shahid"]
+
+for (let i = 0; i < username.length; i++) {
+
+    if(username[i] === "admin"){
+        console.log("Hello admin, would you like to see a status report?");
+    }else{
+        console.log(` Hello ${username[i]}, thank you for logging in again.\n`);
+    }
+    
+}
