b73f8f0a93bf06aa91e277b88a27470571caa57a
Sid-Taha <twahasiddqui11@gmail.com>
2024-05-23 01:27:55 +0500

    Q31

---
+++ Q31/index.ts
@@ -0,0 +1,13 @@
+
+
+
+let username: string[] = ["ahmed", "admin", "taha", "asad", "shahid"]
+
+while (username.length > 0) {
+    username.pop()
+}
+
+if (username.length === 0){
+    console.log("We need to find some users!");
+    
+}
