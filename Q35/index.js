a1d42af94f998b02943a82efeddd1b7e0f9e1a7c
Sid-Taha <twahasiddqui11@gmail.com>
2024-05-24 02:15:24 +0500

    Q35

---
+++ Q35/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let animal_array = ["cat", "dog", "cow"];
+for (let i = 0; i < animal_array.length; i++) {
+    console.log(`A ${animal_array[i]} would make a great pet.`);
+}
+console.log("\nAny of these animals would make a great pet!");
---
+++ Q35/index.ts
@@ -0,0 +1,7 @@
+let animal_array = ["cat", "dog", "cow"]
+
+for (let i = 0; i < animal_array.length; i++) {
+    console.log(`A ${animal_array[i]} would make a great pet.`);
+}
+
+console.log("\nAny of these animals would make a great pet!");
