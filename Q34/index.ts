698a205fdde9c14995563976c1c96962f7c97944
Sid-Taha <twahasiddqui11@gmail.com>
2024-05-24 02:09:13 +0500

    Q34

---
+++ Q34/index.ts
@@ -0,0 +1,7 @@
+let pizza_array = ["B.B.Q", "fajita", "Maxican"]
+
+for (let i = 0; i < pizza_array.length; i++) {
+    console.log(`i like ${pizza_array[i]} pizza.`);
+}
+
+console.log("i really like pizza.");
