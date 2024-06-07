15f45df10ad0e636adf19ed71ab99cec654a3e4e
Sid-Taha <twahasiddqui11@gmail.com>
2024-06-08 03:19:18 +0500

    project 3

--- Node Projects/project 01/index.js
+++ Node Projects/project 01/index.js
@@ -1,33 +1,44 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
-let question = await inquirer.prompt([
-    {
-        name: "ans1",
-        type: "list",
-        message: chalk.redBright.bold("Please select operation: \n"),
-        choices: ["➕", "➖", "➗", "❌"],
-    },
-    {
-        name: "ans2",
-        type: "number",
-        message: chalk.redBright.bold("Please enter your first number: \n"),
-    },
-    {
-        name: "ans3",
-        type: "number",
-        message: chalk.redBright.bold("Please enter your second number: \n"),
-    },
-]);
-if (question.ans1 === "➕") {
-    console.log(chalk.green.bold(`\n\t ➕ Result is ${question.ans2 + question.ans3}`));
-}
-else if (question.ans1 === "➖") {
-    console.log(chalk.green.bold(`\n\t ➖ Result is ${question.ans2 - question.ans3}`));
-}
-else if (question.ans1 === "➗") {
-    console.log(chalk.green.bold(`\n\t ➗ Result is ${question.ans2 / question.ans3}`));
-}
-else if (question.ans1 === "❌") {
-    console.log(chalk.green.bold(`\n\t ❌ Result is ${question.ans2 * question.ans3}`));
-}
-console.log(chalk.hex("#24eb09").bold("taha"));
+let running = true;
+do {
+    let question = await inquirer.prompt([
+        {
+            name: "ans1",
+            type: "list",
+            message: chalk.redBright.bold("Please select operation: \n"),
+            choices: ["➕", "➖", "➗", "❌"],
+        },
+        {
+            name: "ans2",
+            type: "number",
+            message: chalk.redBright.bold("Please enter your first number: \n"),
+        },
+        {
+            name: "ans3",
+            type: "number",
+            message: chalk.redBright.bold("Please enter your second number: \n"),
+        },
+    ]);
+    if (question.ans1 === "➕") {
+        console.log(chalk.green.bold(`\n\t ➕ Result is ${question.ans2 + question.ans3}`));
+    }
+    else if (question.ans1 === "➖") {
+        console.log(chalk.green.bold(`\n\t ➖ Result is ${question.ans2 - question.ans3}`));
+    }
+    else if (question.ans1 === "➗") {
+        console.log(chalk.green.bold(`\n\t ➗ Result is ${question.ans2 / question.ans3}`));
+    }
+    else if (question.ans1 === "❌") {
+        console.log(chalk.green.bold(`\n\t ❌ Result is ${question.ans2 * question.ans3}`));
+    }
+    let confirm = await inquirer.prompt({
+        name: "ans",
+        type: "confirm",
+        message: chalk.redBright.bold("Do you want to continue? ")
+    });
+    if (confirm.ans === false) {
+        running = false;
+    }
+} while (running);
+console.log(chalk.hex("#24eb09").bold("\n\tThank you for using calculator 😡"));
---
+++ Node Projects/project 03/index.js
@@ -0,0 +1,88 @@
+// This somewhat complex TypeScript/Node.js project is a console-based application. When the system starts the user is prompted with a user id and user pin. After entering the details successfully, the ATM functionalities are unlocked. All the user data is generated randomly.
+import inquirer from "inquirer";
+let id = "";
+let pin = 0;
+let Account_balance = 0;
+let running = true;
+//----------------------------------------user id -------------------------
+let question1 = await inquirer.prompt({
+    name: "id",
+    type: "input",
+    message: "Please enter your name ?"
+});
+id = question1.id;
+//----------------------------------------pin-------------------------
+let question2 = await inquirer.prompt({
+    name: "pin",
+    type: "number",
+    message: "Please generate your 4 digit pin !"
+});
+if (question2.pin <= 9999 && question2.pin > 999) {
+    pin = question2.pin;
+}
+else {
+    console.log("You entered more than 4 digits");
+}
+//----------------------------------------message account created-------------------------
+console.log("\n\t🎉Welcome to Bank application, Your account has been created🎉");
+console.log(`\tyour id is: ${id}`);
+console.log(`\tyour ATM pin is: ${pin}\n`);
+do {
+    //----------------------------------------list show-------------------------
+    let options = await inquirer.prompt({
+        name: "list",
+        type: "list",
+        message: "Please select any of one !",
+        choices: ["Balance check", "Amount deposit", "Amount widthdraw"]
+    });
+    //                                                     -------Balance check----------
+    if (options.list === "Balance check") {
+        console.log(`You account balance is ${Account_balance}`);
+    }
+    //                                                     -------Amount deposit----------
+    else if (options.list === "Amount deposit") {
+        let deposit = await inquirer.prompt({
+            name: "Amount",
+            type: "number",
+            message: "Please enter your deposit amount !"
+        });
+        if (deposit.Amount > 0) {
+            Account_balance += deposit.Amount;
+        }
+        else {
+            console.log("Invalid amount");
+        }
+    }
+    //                                                     -------Amount widthdraw----------
+    else if (options.list === "Amount widthdraw") {
+        if (Account_balance > 0) {
+            let widthdraw = await inquirer.prompt({
+                name: "Amount",
+                type: "number",
+                message: "Please enter your widthdraw amount !"
+            });
+            if (Account_balance > widthdraw.Amount) {
+                Account_balance -= widthdraw.Amount;
+                console.log(`You new balance is Rs:${Account_balance}`);
+            }
+            else {
+                console.log("Low balance");
+            }
+        }
+        else {
+            console.log(`Your account balance is 0, Please deposite some amount`);
+        }
+    }
+    //----------------------------------------User confirmation -------------------------
+    let confirm = await inquirer.prompt({
+        name: "ans",
+        type: "confirm",
+        message: "Do you want to continue ?",
+        default: true
+    });
+    if (confirm.ans === false) {
+        running = false;
+    }
+} while (running);
+//----------------------------------------Thank you message -------------------------
+console.log("🎉Thank you using ATM machine🎉");
